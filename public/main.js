ab0c5ce69ac12550f275be1716f13852c2b06f39
Nathanael Swander <swander926@yahoo.com>
2019-11-12 21:48:13 -0500

    Enter zip, city, country to find weather temp

--- public/main.js
+++ public/main.js
@@ -1,7 +1,20 @@
-const main = () => {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
-  }
+let weatherData
+
+const getWeather = async () => {
+  const userInput = document.querySelector('.weather-input').value
+  console.log('going out to api')
+  const url =
+    'https://api.openweathermap.org/data/2.5/weather?q=' +
+    userInput +
+    '&units=imperial&appid=de4ef1f1269489087794b38d35cbc46f'
+  const response = await fetch(url)
+  console.log('back from api')
+  weatherData = await response.json()
+  console.log(weatherData)
+  displayData(weatherData)
 }
 
-document.addEventListener('DOMContentLoaded', main)
+const displayData = weatherData => {
+  document.querySelector('.data').textContent = weatherData.main.temp
+}
+document.querySelector('.weathercheck').addEventListener('click', getWeather)
